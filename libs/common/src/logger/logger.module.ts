d6b004a9b140823a10b48bd471eb85e33eb0c5a3
Ricardo <ricardo85x@gmai.com>
2024-01-05 19:24:28 -0300

    feat: log on single line

--- libs/common/src/logger/logger.module.ts
+++ libs/common/src/logger/logger.module.ts
@@ -8,7 +8,7 @@ import { LoggerModule as PinoLoggerModule } from 'nestjs-pino';
         transport: {
           target: 'pino-pretty',
           options: {
-            // singleLine: true,
+            singleLine: true,
           },
         },
       },
